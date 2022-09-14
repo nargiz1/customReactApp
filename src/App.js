5c2f2a1cae25439c4bea305a595fd1c0de347d5d
nargiz1 <nargizer@code.edu.az>
2022-09-15 01:59:49 +0400

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import thumb from './th.jpg'
+
+const App =()=> (
+    <div><h1 className='wow'>Hello World!</h1>
+        <img src={thumb} />
+    </div>
+)
+
+export default App
